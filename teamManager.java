a8033522c992d1d916d9bbcf2f60a48ca1be498a
Roselyne <roselynemakena@gmail.com>
2015-09-24 17:09:12 +0000

    Added scaffold teamManager.java file - Initial class commit

---
+++ teamManager.java
@@ -0,0 +1,36 @@
+
+
+//Main controller method
+public class teamManager{
+
+	public static void (String[] args){
+		//Main method
+	}
+
+
+
+}
+//Handles all validation processes
+class validation{
+
+
+}
+
+//Handles group creations and instances
+class groupManipulator
+{
+
+}
+
+//Handles IO of files - Read/Write Cycles
+class fileHandler{
+
+}
+
+//Handles all logging/audits in manipiulating the app.
+class logGenerator()
+{
+
+}
+
+
