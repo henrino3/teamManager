eddc7567065faad4bd375fbd5ab647f05a20a182
Kate Kimani <katewkimani@gmail.com>
2015-09-25 08:28:17 +0000

    Adds the validation class.

---
+++ validateMethods.java
@@ -0,0 +1,23 @@
+public class validateMethods{
+  public boolean validateFellow(String name){
+    if exists return true;
+    else return false;
+  }
+
+  public boolean validateEIT(String name, String group, String project){
+    if exists return false;
+    else return true;
+  }
+
+  public boolean validateGroup(String name, String project){
+    if exists return false;
+    else return true;
+  }
+
+  public boolean checkSize(String group, int size){
+    if size < 2 or size > groupArray.length()
+      return false;
+    else return true;
+  }
+
+}
